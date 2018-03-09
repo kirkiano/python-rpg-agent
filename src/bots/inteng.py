fcd5ea12636fcfd6b48fa139273f3324d0a04a56
Kirkian O <kirkiano137@gmail.com>
2018-03-09 12:14:49 -0500

    debug inteng

--- src/bots/inteng.py
+++ src/bots/inteng.py
@@ -8,6 +8,11 @@ def download_interesting_engineering(debug=False):
     r = get_web_page('https://interestingengineering.com', debug=debug)
     soup = BeautifulSoup(r, 'html.parser')
     tags = []
-    for cls in ('main-post-title', 'main-video-title', 'featured-image'):
-        tags += soup.find_all('a', {'class': cls})
-    return [dict(id=uuid4(), title=t['title']) for t in tags]
+    for cls, get_title in (
+            ('main-post-title', lambda t: t['title']),
+            ('main-video-title', lambda t: t.text),
+            ('featured-image', lambda t: t['title'])):
+        tags += [dict(id=uuid4(), title=get_title(t))
+                 for t in soup.find_all('a', {'class': cls})]
+
+    return tags
