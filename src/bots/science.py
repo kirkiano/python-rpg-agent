cd0c67687be318f2a3d9ea6410d3162126b73678
Kirkian O <kirkiano137@gmail.com>
2018-06-20 06:31:25 -0400

    cure some single-letter vars

--- src/bots/science.py
+++ src/bots/science.py
@@ -6,8 +6,8 @@ from web import get_web_page
 
 
 async def scrape_sciencemag():
-    r = await get_web_page('http://www.sciencemag.org/')
-    soup = BeautifulSoup(r, 'html.parser')
+    html = await get_web_page('http://www.sciencemag.org/')
+    soup = BeautifulSoup(html, 'html.parser')
     tags = soup.find_all(class_=lambda a: a and a.startswith('media__headline'))
-    headlines = [dict(id=uuid4(), title=t.text) for t in tags]
+    headlines = [dict(id=uuid4(), title=tag.text) for tag in tags]
     return headlines
