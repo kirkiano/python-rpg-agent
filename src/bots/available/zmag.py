c65912fe130ad837f516227406c6cb72ecf8da55
Kirkian O <kirkiano137@gmail.com>
2018-07-23 22:30:20 -0400

    fix bugs in zmag and counterpunch

--- src/bots/available/zmag.py
+++ src/bots/available/zmag.py
@@ -5,11 +5,11 @@ from scraper import html_scraper
 
 @html_scraper('https://zcomm.org/zmag')
 def scrape_zmag(soup):
-    widgets = soup.find_all({'class': 'zmagwidget'})
+    widgets = soup.find_all('div', {'class': 'zmagwidget'})
     [current_zmag] = [w for w in widgets
                       if w.find('h5') and
                       w.find('h5').text.lower() == 'current zmag']
-    return [dict(id=uuid4(), title=' '.join(*t.text.split('\n\t')))
+    return [dict(id=uuid4(), title=' '.join(t.text.split('\n\t')))
             for t in current_zmag.find_all('div', {'class': 'zmagtoctitles'})]
 
 
