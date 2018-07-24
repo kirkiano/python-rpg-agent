2657e844013a840311c65f651aa1f084d73f6e1a
Kirkian O <kirkiano137@gmail.com>
2018-07-23 22:30:20 -0400

    fix bugs in zmag and counterpunch

--- src/bots/available/counterpunch.py
+++ src/bots/available/counterpunch.py
@@ -6,8 +6,8 @@ from scraper import html_scraper
 @html_scraper('https://www.counterpunch.org')
 def scrape_counterpunch(soup):
     def title_then_author(tag):
-        author = tag.find('left-sidebar-author').text
-        title = tag.find('left-sidebar-title').text
+        author = tag.find(class_='left-sidebar-author').text
+        title = tag.find(class_='left-sidebar-title').text
         return f'{title}, by {author}'
 
     return [dict(id=uuid4(), title=title_then_author(t))
