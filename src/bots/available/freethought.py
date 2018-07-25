4c5d79f7cc8e396dee6b79e809c9feb1adb8cbc3
Kirkian O <kirkiano137@gmail.com>
2018-07-24 21:04:17 -0400

    debug freethought

--- src/bots/available/freethought.py
+++ src/bots/available/freethought.py
@@ -5,8 +5,8 @@ from scraper import html_scraper
 
 @html_scraper('https://thefreethoughtproject.com')
 def scrape_freethought(soup):
-    def select(a):
-        return a and a.startswith('td_module_trending_now')
-
     return [dict(id=uuid4(), title=t.text)
-            for t in soup.find_all('div', {'class': select})]
+            for t in soup.find_all(class_='entry-title')]
+
+
+SCRAPERS = [scrape_freethought]
