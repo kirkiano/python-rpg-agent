043da6ce047ee7982400033973f5f3c7b52ffecf
Kirkian O <kirkiano137@gmail.com>
2018-07-24 20:05:42 -0400

    Small improvements
    * name APS scrapers
    * drop prfluids, prapplied, prmaterials
    * improve physics today
    * add tomdispatch
    * improve zerohedge

--- src/bots/available/zerohedge.py
+++ src/bots/available/zerohedge.py
@@ -5,7 +5,7 @@ from scraper import html_scraper
 
 @html_scraper('https://www.zerohedge.com')
 def scrape_zerohedge(soup):
-    tags = soup.find_all('article')
+    tags = soup.find_all(class_='field--name-title')
     return [dict(id=uuid4(), title=t.text) for t in tags]
 
 
