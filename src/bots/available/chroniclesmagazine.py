eee8dfe0544130eddc92b6d0ee39b99d966d7222
Kirkian O <kirkiano137@gmail.com>
2018-10-10 17:38:00 -0400

    add another bot

---
+++ src/bots/available/chroniclesmagazine.py
@@ -0,0 +1,14 @@
+from uuid import uuid4
+
+from scraper import html_scraper
+
+
+@html_scraper('https://chroniclesmagazine.org')
+def scrape_chroniclesmagazine(soup):
+    contents = soup.find_all('div', {'class': 'content'})
+    slide_infos = soup.find_all('div', {'class': 'slideInfo'})
+    tags = contents + slide_infos
+    return [dict(id=uuid4(), title=t.text) for t in tags]
+
+
+SCRAPERS = [scrape_chroniclesmagazine]
